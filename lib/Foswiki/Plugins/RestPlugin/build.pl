bc178631f2c93b7ff98378e6f5b1b36b511d2684
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-02-25 06:16:20 +0000

    Item1143: ported, ready for testing and development
    git-svn-id: http://svn.foswiki.org/trunk/RestPlugin@2700 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/RestPlugin/build.pl
+++ lib/Foswiki/Plugins/RestPlugin/build.pl
@@ -1,23 +1,18 @@
 #!/usr/bin/perl -w
 BEGIN {
-    unshift @INC, split( /:/, $ENV{TWIKI_LIBS} );
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 use Foswiki::Contrib::Build;
 
 # Create the build object
 $build = new Foswiki::Contrib::Build('RestPlugin');
 
-# (Optional) Set the details of the repository for uploads.
-# This can be any web on any accessible TWiki installation.
-# These defaults will be used when expanding tokens in .txt
-# files, but be warned, they can be overridden at upload time!
-
 # name of web to upload to
 $build->{UPLOADTARGETWEB} = 'Plugins';
 # Full URL of pub directory
-$build->{UPLOADTARGETPUB} = 'http://twiki.org/p/pub';
+$build->{UPLOADTARGETPUB} = 'http://foswiki.org/pub';
 # Full URL of bin directory
-$build->{UPLOADTARGETSCRIPT} = 'http://twiki.org/cgi-bin';
+$build->{UPLOADTARGETSCRIPT} = 'http://foswiki.org/bin';
 # Script extension
 $build->{UPLOADTARGETSUFFIX} = '';
 
